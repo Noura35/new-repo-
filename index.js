6914eb5527665d3debe20e6680b37586f2899f36
Noura35 <belhadjamor.noura1999@gmail.com>
2022-06-09 11:55:14 +0200

    first

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ app.post('/api/sensors', function(req, res) {
 
 
 // start the server @localhost:5000
-const port = process.env.PORT || 5000;
-app.listen(port, function() {
+const port = process.env.PORT ||4000;
+app.listen(port, () =>{
     console.log(`Server started on port ${port}`);
 });
 
